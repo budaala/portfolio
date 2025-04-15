f48379e86ffd8da6a4209ae74f82477f21eb5b6a
budaala <budaala4@gmail.com>
2025-04-15 12:24:47 +0200

    added background blobs and fixed styling

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -4,9 +4,9 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 </script>
 
 <template>
-    <footer class="mt-30 text-sm text-gray-400"
-        style="background: linear-gradient(-180deg, #05041e 20%, #6320ee 230%);">
-        <div class="max-w-6xl mx-auto px-4 pt-8 flex flex-row flex-wrap justify-between items-baseline sm:gap-4 pb-3">
+    <footer>
+        <div
+            class="max-w-6xl mx-auto px-4 pt-8 flex flex-row flex-wrap justify-between items-baseline sm:gap-4 pb-3 z-[2]">
             <div class="text-left">
                 <p>Thanks for scrolling! <br> Hope you enjoyed my work 👋</p>
             </div>
@@ -17,7 +17,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
             </a>
         </div>
 
-        <div class="max-w-6xl mx-auto px-4 pb-8 text-xs text-gray-400">
+        <div class="max-w-6xl mx-auto px-4 pb-8 text-xs">
             <hr class="border-gray-700 mb-4" />
             Copyright © Alicja Buda 2025
         </div>
